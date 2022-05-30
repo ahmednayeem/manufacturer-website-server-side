d9d74fded0e81616a9da1477637aefd3d1adfd84
ahmednayeeme <ahmednaim353@gmail.com>
2022-05-30 16:12:01 +0600

    product load done

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5a0yrku.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    try{
+        await client.connect();
+        const productCollection = client.db('motorbike').collection('products');
+
+        app.get('/product', async(req,res)=>{
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
-  res.send('Hello From assignment-12')
+  res.send('Hello From motorbike')
 })
 
 app.listen(port, () => {
-  console.log(`Assignment-12 app listening on port ${port}`)
+  console.log(`motorbike app listening on port ${port}`)
 })
